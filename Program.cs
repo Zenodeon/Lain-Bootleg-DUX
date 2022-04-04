e18a2c4b490a1f60ea715c2418fc9b34277d33f7
Zenodeon <32438218+Zenodeon@users.noreply.github.com>
2022-04-04 16:27:44 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lain_Bootleg_DUX
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
